3142989a9431d0317b55c6213c7eb7c04b384703
dkhamsing <danielkhamsing@gmail.com>
2014-03-13 20:47:23 -0700

    Add documentation for test, blocking, users, account, favorites

--- FHSTwitterEngine/FHSTwitterEngine.h
+++ FHSTwitterEngine/FHSTwitterEngine.h
@@ -58,16 +58,17 @@
 #pragma mark - Statuses/update
 
 /**
- Posts a tweet.
+ Posts a Tweet.
  @param tweetString Tweet to post.
  @return If an error occurs, returns an NSError object that describes the problem.
  */
 - (NSError *)postTweet:(NSString *)tweetString;
 
+
 /**
- Posts a reply to a tweet.
+ Posts a reply to a Tweet.
  @param tweetString Reply.
- @param inReplyToString Tweet ID to reply to
+ @param inReplyToString Tweet ID to reply to.
  @return If an error occurs, returns an NSError object that describes the problem.
  */
 - (NSError *)postTweet:(NSString *)tweetString inReplyTo:(NSString *)inReplyToString;
@@ -76,59 +77,202 @@
 #pragma mark - Statuses/home_timeline
 
 /**
- Gets a timeline of tweets for authenticated user.
- @param sinceID Timeline will return tweets with ID greater than this value (optional)
- @param count Number of tweets to retrieve
- @return Timeline of tweets
+ Gets a timeline of Tweets for the authenticated user.
+ @param sinceID Timeline will return Tweets with ID greater than this value (optional, set `sinceID` to `@""` if you do not wish to use this parameter).
+ @param count Number of Tweets to retrieve (needs to be greater than zero).
+ @return List of Tweets.
  */
 - (id)getHomeTimelineSinceID:(NSString *)sinceID count:(int)count;
 
-// help/test
+
+#pragma mark - Help/test
+
+/**
+ Tests the Twitter service.
+ */
 - (id)testService;
 
-// blocks/create
+
+#pragma mark - Blocking
+#pragma mark Blocks/create
+
+/**
+ Blocks the user with the given screen name.
+ @param username Screen name of user to block.
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)block:(NSString *)username;
 
-// blocks/destroy
+
+#pragma mark Blocks/destroy
+/**
+ Unblocks the user with the given screen name.
+ @param username Screen name of user to block.
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)unblock:(NSString *)username;
 
-// users/lookup
+
+#pragma mark - Users
+#pragma mark Users/lookup
+
+/**
+ Looks up users.
+ @param users List of users to look up.
+ @param areIDs A boolean value that determines whether the list is of screen names or user IDs. If `YES`, `users` is a list of user IDs.
+ */
 - (id)lookupUsers:(NSArray *)users areIDs:(BOOL)areIDs;
 
-// users/search
+
+#pragma mark Users/search
+
+/**
+ Searches users with the given query.
+ @param q Query of search.
+ @param count Count of query.
+ */
 - (id)searchUsersWithQuery:(NSString *)q andCount:(int)count;
 
-// account/update_profile_image
+
+#pragma mark - Account
+#pragma mark Account/update_profile_image
+
+/**
+ Sets the profile image with an image file path.
+ @param file File path of image.
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)setProfileImageWithImageAtPath:(NSString *)file;
+
+
+/**
+ Sets the profile image with an image.
+ @param data Image data.
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)setProfileImageWithImageData:(NSData *)data;
 
-// account/settings GET and POST
-// See FHSTwitterEngine.m For details
+
+#pragma mark Account/settings GET and POST
+
+/**
+ Gets the authenticated user settings.
+ */
 - (id)getUserSettings;
+
+
+/**
+ Sets the authenticated user settings.
+ @param settings Dictionary with the following keys (values are strings):
+ `sleep_time_enabled`: true/false,
+ `sleep_time_enabled`: true/false,
+ `start_sleep_time`: UTC time,
+ `end_sleep_time`: UTC time,
+ `time_zone`: Europe/Copenhagen, Pacific/Tongatapu,
+ `lang`: en, it, es.
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)updateSettingsWithDictionary:(NSDictionary *)settings;
 
-// account/update_profile
-// See FHSTwitterEngine.m for details
+
+#pragma mark Account/update_profile
+
+/**
+ Updates the profile for the authenticated user.
+ @param settings Dictionary with the following keys (values are strings):
+ `name` (20 characters maximum),
+ `url` (100 characters maximum),
+ `location` (30 characters maximum),
+ `description` (160 characters maximum).
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)updateUserProfileWithDictionary:(NSDictionary *)settings;
 
-// account/update_profile_background_image
+
+#pragma mark Account/update_profile_background_image
+
+/**
+ Sets the profile background image with an image.
+ @param data Image data.
+ @param tiled A boolean value that determines if the image is tiled. If `YES`, the image is tiled.
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)setProfileBackgroundImageWithImageData:(NSData *)data tiled:(BOOL)isTiled;
+
+
+/**
+ Sets the profile background image with an image file path.
+ @param file File path of image.
+ @param tiled A boolean value that determines if the image is tiled. If `YES`, the image is tiled.
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)setProfileBackgroundImageWithImageAtPath:(NSString *)file tiled:(BOOL)isTiled;
+
+
+/**
+ Sets whether the profile uses a background image.
+ @param shouldUseProfileBackgroundImage A boolean value that determines if whether the profile uses a background image.
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)setUseProfileBackgroundImage:(BOOL)shouldUseProfileBackgroundImage;
 
-// account/update_profile_colors
-// See FHSTwitterEngine.m for details
-// If the dictionary is nil, FHSTwitterEngine resets the values
+
+#pragma mark Account/update_profile_colors
+
+/**
+ Sets the profile colors for the authenticated user.
+ @param dictionary Dictionary with the following keys (values are hex colors in string format):
+ `profile_background_color`,
+ `profile_link_color`,
+ `profile_sidebar_border_color`,
+ `profile_sidebar_fill_color`,
+ `profile_text_color`.
+ If the dictionary is nil, FHSTwitterEngine resets the values.
+ @return If an error occurs, returns an NSError object that describes the problem.
+ */
 - (NSError *)updateProfileColorsWithDictionary:(NSDictionary *)dictionary;
 
-// application/rate_limit_status
+
+#pragma mark - Application/rate_limit_status
+
+/**
+ Gets the rate limit status.
+ */
 - (id)getRateLimitStatus;
 
-// favorites/create, favorites/destroy
+
+#pragma mark - Favorites
+#pragma mark Favorites/create, favorites/destroy
+
+/**
+ Marks or unmarks Tweet as favorite.
+ @param tweetID ID of Tweet to mark or unmark as favorite.
+ @param flag A boolean value that determines if the Tweet is to be marked or unmarked as favorite. If `YES`, the Tweet is marked as favorite.
+ */
 - (NSError *)markTweet:(NSString *)tweetID asFavorite:(BOOL)flag;
 
-// favorites/list
+
+#pragma mark Favorites/list
+
+/**
+ Gets the favorite Tweets for a given user.
+ @param user Screen name of user ID.
+ @param isID A boolean that determines of `user` is a screen name or a user ID.
+ @param count Number of Tweets to get.
+ @return A list of Tweets favorited by the user.
+ */
 - (id)getFavoritesForUser:(NSString *)user isID:(BOOL)isID andCount:(int)count;
+
+
+/**
+ Gets the Tweets favorited by a given user.
+ @param user Screen name of user ID.
+ @param isID A boolean that determines of `user` is a screen name or a user ID.
+ @param count Specifies the number of records to retrieve. Must be less than or equal to 200. Defaults to 20.
+ @param sinceID Returns results with an ID greater than (that is, more recent than) the specified ID. There are limits to the number of Tweets which can be accessed through the API. If the limit of Tweets has occured since the since_id, the since_id will be forced to the oldest ID available.
+ @param maxID Returns results with an ID less than (that is, older than) or equal to the specified ID.
+ @return A list of Tweets.
+ */
 - (id)getFavoritesForUser:(NSString *)user isID:(BOOL)isID andCount:(int)count sinceID:(NSString *)sinceID maxID:(NSString *)maxID;
 
 // account/verify_credentials
