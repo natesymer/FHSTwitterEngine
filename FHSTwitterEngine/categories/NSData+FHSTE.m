771c9591c7ab669e2e7e89d6ed35223f21c15d82
Nathaniel Symer <nate@natesymer.com>
2014-04-04 19:19:38 -0400

    clean up NSData category to remove slow base64 iOS 6 compatibility code.

--- FHSTwitterEngine/categories/NSData+FHSTE.m
+++ FHSTwitterEngine/categories/NSData+FHSTE.m
@@ -8,8 +8,6 @@
 
 #import "NSData+FHSTE.h"
 
-static char const Encode[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-
 @implementation NSData (FHSTwitterEngine)
 
 - (NSString *)appropriateFileExtension {
@@ -26,61 +24,20 @@ static char const Encode[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwx
         case 0x49:
         case 0x4D:
             return @"tiff";
+        default:
+            return nil;
     }
-    return nil;
 }
 
 - (NSString *)base64Encode {
     if ([self respondsToSelector:@selector(base64EncodedStringWithOptions:)]) {
         return [self base64EncodedStringWithOptions:0];
+    } else {
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+        return [self base64Encoding];
+#pragma GCC diagnostic pop
     }
-    
-    int outLength = ((((self.length*4)/3)/4)*4)+(((self.length*4)/3)%4?4:0);
-    const char *inputBuffer = self.bytes;
-    char *outputBuffer = malloc(outLength+1);
-    outputBuffer[outLength] = 0;
-    
-    int cycle = 0;
-    int inpos = 0;
-    int outpos = 0;
-    char temp;
-    
-    outputBuffer[outLength-1] = '=';
-    outputBuffer[outLength-2] = '=';
-    
-    while (inpos < self.length) {
-        switch (cycle) {
-            case 0:
-                outputBuffer[outpos++] = Encode[(inputBuffer[inpos]&0xFC)>>2];
-                cycle = 1;
-                break;
-            case 1:
-                temp = (inputBuffer[inpos++]&0x03)<<4;
-                outputBuffer[outpos] = Encode[temp];
-                cycle = 2;
-                break;
-            case 2:
-                outputBuffer[outpos++] = Encode[temp|(inputBuffer[inpos]&0xF0)>>4];
-                temp = (inputBuffer[inpos++]&0x0F)<<2;
-                outputBuffer[outpos] = Encode[temp];
-                cycle = 3;
-                break;
-            case 3:
-                outputBuffer[outpos++] = Encode[temp|(inputBuffer[inpos]&0xC0)>>6];
-                cycle = 4;
-                break;
-            case 4:
-                outputBuffer[outpos++] = Encode[inputBuffer[inpos++]&0x3f];
-                cycle = 0;
-                break;
-            default:
-                cycle = 0;
-                break;
-        }
-    }
-    NSString *pictemp = [NSString stringWithUTF8String:outputBuffer];
-    free(outputBuffer);
-    return pictemp;
 }
 
 @end
