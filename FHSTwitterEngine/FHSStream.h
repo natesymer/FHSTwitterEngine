85110678e7a5ed7ecc949616329bfe4bc0ea0878
dkhamsing <dkhamsing@cirruspath.com>
2015-12-08 10:47:18 -0800

    [doc] Document FHSStream

--- FHSTwitterEngine/FHSStream.h
+++ FHSTwitterEngine/FHSStream.h
@@ -9,13 +9,33 @@
 #import <Foundation/Foundation.h>
 #import "FHSTwitterEngine.h"
 
+/** Stream. */
 @interface FHSStream : NSObject
 
+/**
+ Stream block.
+ */
 @property (nonatomic, copy) StreamBlock block;
 
+/**
+ Stream with URL.
+ @param url Stream URL.
+ @param httpMethod HTTP method.
+ @param params Parameters.
+ @param timeout Time out value.
+ @param block StreamBlock block.
+ @return A stream instance.
+ */
 + (FHSStream *)streamWithURL:(NSString *)url httpMethod:(NSString *)httpMethod parameters:(NSDictionary *)params timeout:(float)timeout block:(StreamBlock)block;
 
+/**
+ Start stream.
+ */
 - (void)stop;
+
+/**
+ Stop stream.
+ */
 - (void)start;
 
 @end
